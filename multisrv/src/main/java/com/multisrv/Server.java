d6fe9ebc426c2b6d2b1b7bc11cfcc07d56e671b0
Chris Vrekos <172226988+ChrisVrekos@users.noreply.github.com>
2025-03-23 10:06:40 +0200

    Improve server startup error handling and enhance connection logging

--- multisrv/src/main/java/com/multisrv/Server.java
+++ multisrv/src/main/java/com/multisrv/Server.java
@@ -7,23 +7,29 @@ public class Server {
         public static void main(String[] args) throws IOException
         {
             int port = args.length > 0 ? Integer.parseInt(args[0]) : 5058;
-            ServerSocket ss = new ServerSocket(port);
-            while(true)
-            {
-                Socket s = null;
-                try {
-                    s = ss.accept();
-                    System.out.println("A new client connected" + s.toString());
+            try (ServerSocket ss = new ServerSocket(port)) {
+                System.out.println("Server is running on port " + port);
 
-                    DataInputStream clin = new DataInputStream(s.getInputStream());
-                    DataOutputStream clout = new DataOutputStream(s.getOutputStream());
-                    System.out.println("Detaching client to thread \n");
-                    Thread t = new ClientHandler(s, clin, clout);
-                    t.start();
-                } catch (Exception e) {
-                    s.close();
-                    e.printStackTrace();
+                while(true)
+                {
+                    Socket s = null;
+                    try {
+                        s = ss.accept();
+                        System.out.println("A new client connected" + s.toString());
+
+                        DataInputStream clin = new DataInputStream(s.getInputStream());
+                        DataOutputStream clout = new DataOutputStream(s.getOutputStream());
+                        System.out.println("Detaching client to thread \n");
+                        Thread t = new ClientHandler(s, clin, clout);
+                        t.start();
+                    } catch (Exception e) {
+                        s.close();
+                        e.printStackTrace();
+                    }
                 }
+            }  catch (IOException e) {
+                System.err.println("Error starting server: " + e.getMessage());
+                e.printStackTrace();
             }
         }
     }
